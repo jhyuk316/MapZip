b61863cc1e8e1ad27908ee2e92b2b57850aceb73
Park JongHyuk <94343223+jhyuk316@users.noreply.github.com>
2022-05-03 22:43:40 +0900

    spring security login page 끄기

--- src/main/java/com/jhyuk316/mapzip/config/SecurityConfig.java
+++ src/main/java/com/jhyuk316/mapzip/config/SecurityConfig.java
@@ -20,7 +20,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
         http
             .csrf().disable()
             .headers().frameOptions().disable()
